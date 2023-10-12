5d144a7367281fe120ace6d5a8f7605d71116693
Gijs Straathof <gijs.straathof@harborn.com>
2023-10-12 09:04:56 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from process import start_configurator, condition_is_met, process_choice, translate
+from process import start_configurator, condition_is_met, process_choice, translate, is_selected
 
-
-st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
 st.markdown(
     """
@@ -19,26 +17,39 @@ button {
 
 tab1, tab2 = st.tabs(["configurator", "result"])
 with tab1:
-    if not st.session_state.get('configurator_in_progress'): 
+    if not st.session_state.get("configurator_in_progress"):
         st.button("Start Configurator", on_click=start_configurator)
 
-    if st.session_state.get('configurator_in_progress'):
-        st.title(translate(st.session_state['configurator']['title']))
+    if st.session_state.get("configurator_in_progress"):
+        st.title(translate(st.session_state["configurator"]["title"]))
 
-        for c in st.session_state['configurator']['content']:
-            if condition_is_met(c['condition']):
-                if c['title']:
-                    st.header(translate(c['title']))
-                if c['subtitle']:
-                    st.subheader(translate(c['subtitle']))
-                if c['type'] == 'choice':
-                    cols = st.columns([1,1,1])
-                    for i, choice in enumerate(c['meta']['choices']):
+        for c in st.session_state["configurator"]["content"]:
+            if condition_is_met(c["condition"]):
+                if c["title"]:
+                    st.header(translate(c["title"]))
+                if c["subtitle"]:
+                    st.subheader(translate(c["subtitle"]))
+                if c["type"] == "choice":
+                    cols = st.columns([1, 1, 1])
+                    for i, choice in enumerate(c["meta"]["choices"]):
                         with cols[i]:
-                            st.button(translate(choice['title']), on_click=process_choice, args=(c['id'], choice['value']))
-                if c['type'] == 'input':
-                    st.number_input(translate(c['meta']['label']), placeholder=translate(c['meta']['placeholder']), min_value=c['meta']['range']['min'], max_value=c['meta']['range']['max'])
+                            st.button(
+                                translate(choice["title"]),
+                                on_click=process_choice,
+                                args=(c["id"], choice["value"]),
+                                disabled=is_selected(c["id"], choice["value"])
+                            )
+                if c["type"] == "input":
+                    st.number_input(
+                        translate(c["meta"]["label"]),
+                        placeholder=translate(c["meta"]["placeholder"]),
+                        min_value=c["meta"]["range"]["min"],
+                        max_value=c["meta"]["range"]["max"],
+                        on_change=process_choice,
+                        args=(c["id"],),
+                        key=c["id"],
+                    )
 
 with tab2:
-    for k, v in st.session_state.get('session_data', {}).items():
+    for k, v in st.session_state.get("session_data", {}).items():
         st.text(f'{k}: "{v}"')
--- process.py
+++ process.py
@@ -1,32 +1,41 @@
 import streamlit as st
 import json
 from copy import deepcopy
-
+from pprint import pprint
 
 
 def start_configurator():
-    st.session_state['configurator_in_progress'] = True
-    st.session_state['configurator'] = json.load(open('configurator.json', 'r'))
-    st.session_state['session_data'] = {}
-    st.session_state['translatioins'] = json.load(open('translations.json', 'r'))
+    st.session_state["configurator_in_progress"] = True
+    st.session_state["configurator"] = json.load(open("configurator.json", "r"))
+    st.session_state["session_data"] = {}
+    st.session_state["translatioins"] = json.load(open("translations.json", "r"))
 
 
-def process_choice(field, value):
-    st.session_state['session_data'][field] = value
+def process_choice(field, value=None):
+    if value == None:
+        value = st.session_state.get(field)
+    st.session_state["session_data"][field] = value
+
+
+def is_selected(field, value):
+    return st.session_state["session_data"].get(field) == value
 
 
 def condition_is_met(condition) -> bool:
     if condition == None:
         return True
-    if condition['type'] == 'equals':
-        return st.session_state['session_data'].get(condition['field']) == condition['value']
-    if condition['type'] == 'and':
-        return all((condition_is_met(c) for c in condition['conditions']))
+    if condition["type"] == "equals":
+        return (
+            st.session_state["session_data"].get(condition["field"])
+            == condition["value"]
+        )
+    if condition["type"] == "and":
+        return all((condition_is_met(c) for c in condition["conditions"]))
 
 
 def translate(key):
-    parts = key.split('.')
-    translations = deepcopy(st.session_state['translatioins'])
+    parts = key.split(".")
+    translations = deepcopy(st.session_state["translatioins"])
     for k in parts:
         translations = translations.get(k)
         if translations == None:
