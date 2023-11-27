0d45979ff178b00a3841209176509371650ff395
Gijs Straathof <gijs.straathof@harborn.com>
2023-11-27 15:52:35 +0100

    Use live url

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
-from process import start_configurator, condition_is_met, process_choice, translate, is_selected
+from process import (
+    start_configurator,
+    condition_is_met,
+    restart_configurator,
+    process_choice,
+    translate,
+    is_selected,
+    process_next,
+    process_back,
+    process_skip,
+)
 
 
 st.markdown(
@@ -18,7 +28,17 @@ button {
 tab1, tab2 = st.tabs(["configurator", "result"])
 with tab1:
     if not st.session_state.get("configurator_in_progress"):
-        st.button("Start Configurator", on_click=start_configurator)
+        st.text_input("Token", key="token", placeholder="ID token")
+        st.markdown("""---""")
+        cols = st.columns([1, 1])
+        with cols[0]:
+            st.text("Start a new configuration")
+            st.text_input("Reference", key="reference", placeholder="your reference")
+            st.button("Start Configurator", on_click=start_configurator)
+        with cols[1]:
+            st.text("Restart an existing configuration")
+            st.text_input("Configuration ID", key="configuration")
+            st.button("Restart Configurator", on_click=restart_configurator)
 
     if st.session_state.get("configurator_in_progress"):
         st.title(translate(st.session_state["configurator"]["title"]))
@@ -30,15 +50,35 @@ with tab1:
                 if c["subtitle"]:
                     st.subheader(translate(c["subtitle"]))
                 if c["type"] == "choice":
-                    cols = st.columns([1, 1, 1])
-                    for i, choice in enumerate(c["meta"]["choices"]):
-                        with cols[i]:
-                            st.button(
-                                translate(choice["title"]),
-                                on_click=process_choice,
-                                args=(c["id"], choice["value"]),
-                                disabled=is_selected(c["id"], choice["value"])
-                            )
+                    if c["meta"]["type"] == "button":
+                        cols = st.columns([1] * len(c["meta"]["choices"]))
+                        for i, choice in enumerate(c["meta"]["choices"]):
+                            with cols[i]:
+                                st.button(
+                                    translate(choice["title"]),
+                                    on_click=process_choice,
+                                    args=(c["id"], choice["value"]),
+                                    disabled=is_selected(c["id"], choice["value"]),
+                                )
+                    if c["meta"]["type"] == "dropdown":
+                        st.selectbox(
+                            "choose",
+                            options=c["meta"]["options"],
+                            on_change=process_choice,
+                            args=(c["id"],),
+                            key=c["id"],
+                        )
+                    if c["meta"]["type"] == "radio":
+                        st.radio(
+                            "choose",
+                            options=[
+                                translate(o["title"]) for o in c["meta"]["choices"]
+                            ],
+                            on_change=process_choice,
+                            args=(c["id"],),
+                            key=c["id"],
+                        )
+
                 if c["type"] == "input":
                     st.number_input(
                         translate(c["meta"]["label"]),
@@ -49,6 +89,26 @@ with tab1:
                         args=(c["id"],),
                         key=c["id"],
                     )
+                if c["type"] == "products":
+                    st.subheader("Products:")
+                    for product in c["meta"]["products"]:
+                        st.text(f"{product['name']} - {product['article_number']}")
+                if c["type"] == "filters":
+                    st.subheader("Filters:")
+                    if "category" in c["meta"]["filters"]:
+                        st.text(f"Category: {c['meta']['filters']['category']}")
+                    if (
+                        "filters" in c["meta"]["filters"]
+                        and c["meta"]["filters"]["filters"]
+                    ):
+                        for k, v in c["meta"]["filters"]["filters"].items():
+                            st.text(f"{k}: {v}")
+
+        with st.sidebar:
+            st.button("Back", on_click=process_back)
+            st.button("Next", on_click=process_next)
+            st.button("Skip", on_click=process_skip)
+
 
 with tab2:
     for k, v in st.session_state.get("session_data", {}).items():
--- process.py
+++ process.py
@@ -1,14 +1,30 @@
 import streamlit as st
 import json
 from copy import deepcopy
-from pprint import pprint
+import requests
+from os import getenv
 
 
 def start_configurator():
     st.session_state["configurator_in_progress"] = True
-    st.session_state["configurator"] = json.load(open("configurator.json", "r"))
+    st.session_state["client"] = PortalCLient(st.session_state["token"])
+    response = st.session_state.client.initiate(st.session_state["reference"])
+    st.session_state["configurator"] = response["state_definition"]
+    st.session_state["configuration_id"] = response["configuration_id"]
     st.session_state["session_data"] = {}
-    st.session_state["translatioins"] = json.load(open("translations.json", "r"))
+    st.session_state["translations"] = json.load(open("translations.json", "r"))
+
+
+def restart_configurator():
+    st.session_state["configurator_in_progress"] = True
+    st.session_state["client"] = PortalCLient(st.session_state["token"])
+    response = st.session_state.client.back(
+        st.session_state["configuration"], "cylinder.settings", {}
+    )
+    st.session_state["configurator"] = response["state_definition"]
+    st.session_state["configuration_id"] = response["configuration_id"]
+    st.session_state["session_data"] = {}
+    st.session_state["translations"] = json.load(open("translations.json", "r"))
 
 
 def process_choice(field, value=None):
@@ -35,9 +51,138 @@ def condition_is_met(condition) -> bool:
 
 def translate(key):
     parts = key.split(".")
-    translations = deepcopy(st.session_state["translatioins"])
+    translations = deepcopy(st.session_state["translations"])
     for k in parts:
         translations = translations.get(k)
         if translations == None:
             return key
     return translations
+
+
+def process_next():
+    response = st.session_state.client.next(
+        st.session_state["configuration_id"],
+        st.session_state.configurator["id"],
+        st.session_state["session_data"],
+    )
+    st.session_state["configurator"] = response["state_definition"]
+    st.session_state["configuration_id"] = response["configuration_id"]
+    st.session_state["session_data"] = {}
+    if st.session_state.configurator["type"] == "productlist":
+        if not has_products(st.session_state.configurator):
+            filters = get_filters(st.session_state.configurator)
+            response = st.session_state.client.get_products(filters)
+            add_products(st.session_state.configurator, response["products"])
+
+
+def process_back():
+    response = st.session_state.client.back(
+        st.session_state["configuration_id"],
+        st.session_state.configurator["id"],
+        st.session_state["session_data"],
+    )
+    st.session_state["configurator"] = response["state_definition"]
+    st.session_state["configuration_id"] = response["configuration_id"]
+    st.session_state["session_data"] = {}
+    if st.session_state.configurator["type"] == "productlist":
+        if not has_products(st.session_state.configurator):
+            filters = get_filters(st.session_state.configurator)
+            print(f"Collecting products with filters: {filters}")
+            response = st.session_state.client.get_products(filters)
+            add_products(st.session_state.configurator, response["products"])
+
+
+def process_skip():
+    response = st.session_state.client.skip(
+        st.session_state["configuration_id"], st.session_state.configurator["id"]
+    )
+    st.session_state["configurator"] = response["state_definition"]
+    st.session_state["configuration_id"] = response["configuration_id"]
+    st.session_state["session_data"] = {}
+
+
+def has_products(state_definition: dict) -> bool:
+    for c in state_definition["content"]:
+        if c["type"] == "products":
+            return len(c["meta"]["products"]) > 0
+    return False
+
+
+def get_filters(state_definition: dict):
+    if state_definition["type"] == "productlist":
+        for c in state_definition["content"]:
+            if c["type"] == "filters":
+                return c["meta"]["filters"]
+
+
+def add_products(state_definition: dict, products: list):
+    for c in state_definition["content"]:
+        if c["type"] == "products":
+            c["meta"]["products"] = products
+
+
+class PortalCLient:
+    base_url = getenv("BASE_URL")
+
+    def __init__(self, token):
+        self.token = token
+
+    def initiate(self, reference):
+        response = requests.post(
+            f"{self.base_url}/configurator/initiate",
+            headers={"Authorization": f"Bearer {self.token}"},
+            json={"reference": reference},
+        )
+        return response.json()
+
+    def next(self, configuration_id, state_id, data):
+        data = {
+            "configuration_id": configuration_id,
+            "state_id": state_id,
+            "configuration_data": [
+                {"attribute_id": k, "value": v} for k, v in data.items()
+            ],
+        }
+        response = requests.post(
+            f"{self.base_url}/configurator/next",
+            headers={"Authorization": f"Bearer {self.token}"},
+            json=data,
+        )
+        return response.json()
+
+    def back(self, configuration_id, state_id, data):
+        data = {
+            "configuration_id": configuration_id,
+            "state_id": state_id,
+            "configuration_data": [
+                {"attribute_id": k, "value": v} for k, v in data.items()
+            ],
+        }
+        response = requests.post(
+            f"{self.base_url}/configurator/back",
+            headers={"Authorization": f"Bearer {self.token}"},
+            json=data,
+        )
+        return response.json()
+
+    def skip(self, configuration_id, state_id):
+        data = {
+            "configuration_id": configuration_id,
+            "state_id": state_id,
+            "configuration_data": [],
+        }
+        response = requests.post(
+            f"{self.base_url}/configurator/skip",
+            headers={"Authorization": f"Bearer {self.token}"},
+            json=data,
+        )
+        return response.json()
+
+    def get_products(self, filters: dict):
+        data = {"filters": filters}
+        response = requests.post(
+            f"{self.base_url}/products",
+            headers={"Authorization": f"Bearer {self.token}"},
+            json=data,
+        )
+        return response.json()
